72a6d7c469188dd14e297deb26e11c0d595a49df
objektwerks <objektwerks@runbox.com>
2023-02-10 09:31:24 -0500

    added login register view

---
+++ client/src/main/scala/pool/LoginRegisterView.scala
@@ -0,0 +1,12 @@
+package pool
+
+import com.raquo.laminar.api.L.*
+
+import org.scalajs.dom.console.log
+
+import Components.*
+
+object LoginRegisterView extends View:
+  def apply(): HtmlElement =
+    div(
+    )
