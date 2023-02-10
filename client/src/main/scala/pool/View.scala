87b6777a385990f2896534fd3d21d01b2006e12c
objektwerks <objektwerks@runbox.com>
2023-02-10 09:32:01 -0500

    added view

---
+++ client/src/main/scala/pool/View.scala
@@ -0,0 +1,19 @@
+package pool
+
+import com.raquo.laminar.api.L.*
+
+enum Mode:
+  case add, edit, readonly
+
+trait View:
+  protected[this] val errorBus = new EventBus[String]
+
+  def call(command: Command, handler: Either[Fault, Event] => Unit): Unit = Fetcher.call(command, handler)
+  
+  def route(page: Page): Unit = PageRouter.router.pushState(page)
+
+  def emit(bus: EventBus[String], message: String): Unit = bus.emit(message)
+
+  def clear(bus: EventBus[String]): Unit = bus.emit("")
+
+  def clearErrorBus(): Unit = errorBus.emit("")
