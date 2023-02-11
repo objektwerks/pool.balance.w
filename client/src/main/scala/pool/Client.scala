eed526171bb526f57f81b1336f35fac30704e371
objektwerks <objektwerks@runbox.com>
2023-02-11 07:29:05 -0500

    added proxy now to client

--- client/src/main/scala/pool/Client.scala
+++ client/src/main/scala/pool/Client.scala
@@ -5,12 +5,14 @@ import com.raquo.laminar.api.L._
 import org.scalajs.dom._
 import org.scalajs.dom.console.log
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 @JSExportTopLevel("Client")
 class Client(rootUrl: String) extends js.Object:
   Urls.set(rootUrl)
+  Proxy.now.foreach(println)
 
   render(
     container = document.getElementById("content"),
