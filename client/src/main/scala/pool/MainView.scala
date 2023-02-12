ecbdd819e4c438ad3c317060979a62fbea0c614b
objektwerks <objektwerks@runbox.com>
2023-02-12 07:40:18 -0500

    built out main view

--- client/src/main/scala/pool/MainView.scala
+++ client/src/main/scala/pool/MainView.scala
@@ -9,4 +9,18 @@ import Components.*
 object MainView extends View:
   def apply(): HtmlElement =
     div(
+      bar(
+        btn("Login").amend {
+          onClick --> { _ =>
+            log("Root -> Login menu item onClick")
+            route(LoginPage)
+          }
+        },
+        rbtn("Register").amend {
+          onClick --> { _ =>
+            log("Root -> Register menu item onClick")
+            route(RegisterPage)
+          }
+        }          
+      )
     )
