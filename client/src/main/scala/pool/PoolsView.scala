4d354a6b9d8e96cd41eb972febbe244c11facf1e
objektwerks <objektwerks@runbox.com>
2023-02-12 08:08:08 -0500

    added pools view

---
+++ client/src/main/scala/pool/PoolsView.scala
@@ -0,0 +1,55 @@
+package pool
+
+import com.raquo.laminar.api.L.*
+
+import org.scalajs.dom.console.log
+
+import Components.*
+
+object PoolsView extends View:
+  def apply(poolsVar: Var[List[Pool]], accountVar: Var[Account]): HtmlElement =
+    def handler(either: Either[Fault, Event]): Unit =
+      either match
+        case Left(fault) => errorBus.emit(s"List pools failed: ${fault.cause}")
+        case Right(event) =>
+          event match
+            case PoolsListed(pools: List[Pool]) =>
+              clearErrors()
+              poolsVar.set(pools)
+            case _ => log(s"Pools -> handler failed: $event")
+
+    div(
+      bar(
+        btn("App").amend {
+          onClick --> { _ =>
+            log("Pools -> App menu item onClick")
+            route(AppPage)
+          }
+        }      
+      ),
+      div(
+        onLoad --> { _ => 
+          val command = ListPools(accountVar.now().license)
+          call(command, handler)
+        },
+        hdr("Pools"),
+        list(
+          split(poolsVar.entitiesVar, (id: Long) => PoolPage(id))
+        )
+      ),
+      cbar(
+        btn("New").amend {
+          onClick --> { _ =>
+            log(s"Pools -> New button onClick")
+            route(PoolPage())
+          }
+        },        
+        btn("Refresh").amend {
+          onClick --> { _ =>
+            log(s"Pools -> Refresh button onClick")
+            val command = ListPools(accountVar.now().license)
+            call(command, handler)
+          }
+        }
+      )
+    )
