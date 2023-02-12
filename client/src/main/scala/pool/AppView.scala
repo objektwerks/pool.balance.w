ecedf74dd125d9db58775c39fe73ce60416cb79a
objektwerks <objektwerks@runbox.com>
2023-02-12 08:02:19 -0500

    added app view

---
+++ client/src/main/scala/pool/AppView.scala
@@ -0,0 +1,28 @@
+package pool
+
+import com.raquo.laminar.api.L.*
+
+import org.scalajs.dom.console.log
+
+import Components.*
+import Validator.*
+
+object AppView extends View:
+  def apply(accountVar: Var[Account]): HtmlElement =
+    div(
+      bar(
+        btn("Account").amend {
+          onClick --> { _ =>
+            log("App -> Account menu item onClick")
+            route(AccountPage)
+          }
+        },
+        rbtn("Pools").amend {
+          disabled <-- accountVar.signal.map { account => account.isDeactivated }
+          onClick --> { _ =>
+            log("App -> Pools menu item onClick")
+            route(PoolsPage)
+          }
+        }          
+      )
+    )
