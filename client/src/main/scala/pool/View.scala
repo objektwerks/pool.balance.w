f533237ea02dbdfe6bd85124a690059c6205c379
objektwerks <objektwerks@runbox.com>
2023-02-13 09:44:36 -0500

    refactored view

--- client/src/main/scala/pool/View.scala
+++ client/src/main/scala/pool/View.scala
@@ -6,14 +6,16 @@ enum Mode:
   case add, edit, readonly
 
 trait View:
-  protected[this] val errorBus = new EventBus[String]
+  private[this] val errorBus = new EventBus[String]
 
   def call(command: Command, handler: Either[Fault, Event] => Unit): Unit = Proxy.call(command, handler)
   
   def route(page: Page): Unit = PageRouter.router.pushState(page)
 
+  def emitError(message: String): Unit = errorBus.emit(message)
+
+  def clearErrors(): Unit = errorBus.emit("")
+
   def emit(bus: EventBus[String], message: String): Unit = bus.emit(message)
 
   def clear(bus: EventBus[String]): Unit = bus.emit("")
-
-  def clearErrors(): Unit = errorBus.emit("")
