8be846877d819fbb2b7d1b335ff9c244e1860b40
objektwerks <objektwerks@runbox.com>
2023-02-14 05:34:07 -0500

    removed save events

--- shared/src/main/scala/pool/Event.scala
+++ shared/src/main/scala/pool/Event.scala
@@ -13,19 +13,15 @@ final case class Reactivated(account: Account) extends Event
 final case class Updated() extends Event
 
 final case class PoolsListed(pools: List[Pool]) extends Event
-final case class PoolSaved(id: Long) extends Event
 final case class PoolAdded(pool: Pool) extends Event
 
 final case class CleaningsListed(cleanings: List[Cleaning]) extends Event
-final case class CleaningSaved(id: Long) extends Event
 final case class CleaningAdded(cleaning: Cleaning) extends Event
 
 final case class MeasurementsListed(measurements: List[Measurement]) extends Event
-final case class MeasurementSaved(id: Long) extends Event
 final case class MeasurementAdded(measurement: Measurement) extends Event
 
 final case class ChemicalsListed(chemicals: List[Chemical]) extends Event
-final case class ChemicalSaved(id: Long) extends Event
 final case class ChemicalAdded(chemical: Chemical) extends Event
 
 final case class Fault(cause: String, occurred: Long = LocalDate.now.toEpochDay) extends Event
