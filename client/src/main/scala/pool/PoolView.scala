535eedcf58f6d8f74a8b671fde5b7945fde8f73d
objektwerks <objektwerks@runbox.com>
2023-02-15 08:36:17 -0500

    refactored pool add handler

--- client/src/main/scala/pool/PoolView.scala
+++ client/src/main/scala/pool/PoolView.scala
@@ -13,16 +13,14 @@ object PoolView extends View:
     val nameErrorBus = new EventBus[String]
     val volumeErrorBus = new EventBus[String]
 
-    def addHandler(either: Either[Fault, Event]): Unit =
-      either match
-        case Left(fault) => emitError(s"Add pool failed: ${fault.cause}")
-        case Right(event) =>
-          event match
-            case PoolAdded(pool) =>
-              clearErrors()
-              model.addEntity(pool)
-              route(PoolsPage)
-            case _ => log(s"Pool -> add handler failed: $event")
+    def addHandler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(s"Add pool failed: $cause")
+        case PoolAdded(pool) =>
+          clearErrors()
+          model.addEntity(pool)
+          route(PoolsPage)
+        case _ => log(s"Pool -> add handler failed: $event")
 
     def updateHandler(either: Either[Fault, Event]): Unit =
       either match
