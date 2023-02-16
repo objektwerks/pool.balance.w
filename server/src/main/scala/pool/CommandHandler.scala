50748e0a22bc86f39fe21fd3cb50eca0a57e5e65
objektwerks <objektwerks@runbox.com>
2023-02-16 08:29:33 -0500

    added fault logger to command handler

--- server/src/main/scala/pool/CommandHandler.scala
+++ server/src/main/scala/pool/CommandHandler.scala
@@ -1,5 +1,7 @@
 package pool
 
+import com.typesafe.scalalogging.LazyLogging
+
 import com.github.plokhotnyuk.jsoniter_scala.core.*
 import com.sun.net.httpserver.{HttpExchange, HttpHandler}
 
@@ -7,12 +9,15 @@ import scala.io.{Codec, Source}
 
 import Serializer.given
 
-final class CommandHandler(dispatcher: Dispatcher) extends HttpHandler:
+final class CommandHandler(dispatcher: Dispatcher) extends HttpHandler with LazyLogging:
   override def handle(exchange: HttpExchange): Unit =
     val json = Source.fromInputStream( exchange.getRequestBody )(Codec.UTF8).mkString("")
     val command = readFromString[Command](json)
     
     val event = dispatcher.dispatch(command)
+    event match
+      case Fault(cause, _) => logger.error(cause)
+      case _ =>
     val response = writeToString[Event](event)
 
     exchange.sendResponseHeaders(200, response.length())
