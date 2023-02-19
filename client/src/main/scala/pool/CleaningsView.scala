b0b3ced2e0e52eafb3d02c043a999bfa653522bc
objektwerks <objektwerks@runbox.com>
2023-02-19 07:57:39 -0500

    added cleanings view

---
+++ client/src/main/scala/pool/CleaningsView.scala
@@ -0,0 +1,51 @@
+package pool
+
+import com.raquo.laminar.api.L.*
+
+import Component.*
+
+object CleaningsView extends View:
+  def apply(model: Model[Cleaning], accountVar: Var[Account]): HtmlElement =
+    def handler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(cause)
+        case CleaningsListed(cleanings: List[Cleaning]) =>
+          clearErrors()
+          model.setEntities(cleanings)
+        case _ => log(s"Cleanings -> handler failed: $event")
+
+    div(
+      bar(
+        btn("Pool").amend {
+          onClick --> { _ =>
+            log("Cleanings -> Pool menu item onClick")
+            route(PoolPage())
+          }
+        }      
+      ),
+      div(
+        onLoad --> { _ => 
+          val command = ListCleanings(accountVar.now().license, 0) // pool id
+          call(command, handler)
+        },
+        hdr("Cleanings"),
+        list(
+          split(model.entitiesVar, (id: Long) => PoolPage(id)) // Cleanings page
+        )
+      ),
+      cbar(
+        btn("New").amend {
+          onClick --> { _ =>
+            log(s"Cleanings -> New button onClick")
+            route(CleaningPage())
+          }
+        },        
+        btn("Refresh").amend {
+          onClick --> { _ =>
+            log(s"Cleanings -> Refresh button onClick")
+            val command = ListCleanings(accountVar.now().license, 0) // pool id
+            call(command, handler)
+          }
+        }
+      )
+    )
