4fbdc9f7ffeaad258e2add35aa70d753fbd0a956
objektwerks <objektwerks@runbox.com>
2023-02-23 13:15:25 -0500

    refactored register view

--- client/src/main/scala/pool/RegisterView.scala
+++ client/src/main/scala/pool/RegisterView.scala
@@ -3,14 +3,11 @@ package pool
 import com.raquo.laminar.api.L.*
 
 import Component.*
-import Error.*
 import Message.*
 import Validator.*
 
 object RegisterView extends View:
   def apply(emailAddressVar: Var[String], pinVar: Var[String], accountVar: Var[Account]): HtmlElement =
-    val emailAddressErrorBus = new EventBus[String]
-
     def handler(event: Event): Unit =
       event match
         case Fault(cause, _) => emitError(cause)
@@ -29,11 +26,7 @@ object RegisterView extends View:
       email.amend {
         value <-- emailAddressVar
         onInput.mapToValue.filter(_.nonEmpty).setAsValue --> emailAddressVar
-        onKeyUp.mapToValue --> { emailAddress =>
-          if emailAddress.isEmailAddress then clear(emailAddressErrorBus) else emit(emailAddressErrorBus, emailAddressError)
-        }
       },
-      err(emailAddressErrorBus),
       cbar(
         btn("Register").amend {
           disabled <-- emailAddressVar.signal.map(email => !email.isEmailAddress)
