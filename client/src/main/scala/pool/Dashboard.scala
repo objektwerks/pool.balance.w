207dc8820654985575272ba2c7be7d6d4ac3448a
objektwerks <objektwerks@runbox.com>
2023-02-24 07:02:33 -0500

    edited dashboard

--- client/src/main/scala/pool/Dashboard.scala
+++ client/src/main/scala/pool/Dashboard.scala
@@ -1,4 +1,14 @@
 package pool
 
+import com.raquo.laminar.api.L.*
+
+/*
+  text = context.headerPh
+  range.text = context.dashboardPhRange
+  good.text = context.dashboardPhGood
+  ideal.text = context.dashboardPhIdeal
+  current.text <== model.currentPh.asString
+  average.text <== model.averagePh.asString
+*/
 object Dashboard:
-  
+  def apply: HtmlElement = ???
