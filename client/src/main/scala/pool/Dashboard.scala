482e98a3e207eedfd0b0c0e7eb604931029a96ab
objektwerks <objektwerks@runbox.com>
2023-02-24 07:03:04 -0500

    edited dashboard

--- client/src/main/scala/pool/Dashboard.scala
+++ client/src/main/scala/pool/Dashboard.scala
@@ -8,7 +8,15 @@ import com.raquo.laminar.api.L.*
   good.text = context.dashboardPhGood
   ideal.text = context.dashboardPhIdeal
   current.text <== model.currentPh.asString
-  average.text <== model.averagePh.asString
+  average.text <== model.averagePh.
+  
+  model.currentPh.onChange { (_, _, newValue) =>
+    if model.phInRange(newValue) then inRangeCurrent else outOfRangeCurrent
+  }
+
+  model.averagePh.onChange { (_, _, newValue) =>
+    if model.phInRange(newValue) then inRangeAverage else outOfRangeAverage
+  }
 */
 object Dashboard:
   def apply: HtmlElement = ???
