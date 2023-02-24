2acfad4fcdcad80d4ea9b79608e62e66277913f1
objektwerks <objektwerks@runbox.com>
2023-02-24 15:10:56 -0500

    exploring chart js

--- client/src/main/scala/pool/Charts.scala
+++ client/src/main/scala/pool/Charts.scala
@@ -3,9 +3,14 @@ package pool
 import scala.scalajs.js
 import scala.scalajs.js.JSConverters.*
 
-import com.raquo.laminar.api.L.*
+import com.raquo.laminar.api.L.{*, given}
 
 import typings.chartJs.mod.*
 
 object Charts:
-  
+  def apply(): HtmlElement =
+    canvasTag (
+      width := "100%",
+      height := "300px"
+
+    )
