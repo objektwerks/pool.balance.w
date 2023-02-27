b26be888cc3c9d2414b9a1908f9723402a03b90f
objektwerks <objektwerks@runbox.com>
2023-02-27 10:35:56 -0500

    edited comment

--- client/src/main/scala/pool/Charts.scala
+++ client/src/main/scala/pool/Charts.scala
@@ -8,8 +8,8 @@ import com.raquo.laminar.api.L.{*, given}
 import typings.chartJs.mod.*
 
 /**
- * Chart.js, via ScalaJs, is not exactly ready for prime
- * time (2023.2.25). Try again later.
+ * Chart.js, via ScalaJs, is not exactly ready
+ * for prime time (2023.2.25). Try again later.
  */
 object Charts:  
   def apply(): HtmlElement = ???
