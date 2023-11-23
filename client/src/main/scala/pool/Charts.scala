f58e06e43360a4f4b80f9873a92b314320e8a536
objektwerks <objektwerks@runbox.com>
2023-11-23 06:34:09 -0500

    edited comments

--- client/src/main/scala/pool/Charts.scala
+++ client/src/main/scala/pool/Charts.scala
@@ -2,9 +2,5 @@ package pool
 
 import com.raquo.laminar.api.L.*
 
-/**
- * Chart.js, via ScalaJs, is not exactly ready
- * for prime time (2023.2.25). Try again later.
- */
 object Charts:  
-  def apply(): HtmlElement = ???
+  def apply(): HtmlElement = ??? // TODO!
