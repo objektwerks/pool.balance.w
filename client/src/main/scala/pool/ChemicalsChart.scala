da4a2c54fd6bcb59aefb430f099d612f0b6cbc5b
objektwerks <objektwerks@runbox.com>
2023-11-29 15:40:53 -0500

    enhanced chemicals chart

--- client/src/main/scala/pool/ChemicalsChart.scala
+++ client/src/main/scala/pool/ChemicalsChart.scala
@@ -7,13 +7,29 @@ import java.time.format.DateTimeFormatter
 
 import ChartBuilder.DataItem
 
+/*
+enum TypeOfChemical(val display: String):
+  case LiquidChlorine extends TypeOfChemical("Liquid Chlorine")
+  case Trichlor extends TypeOfChemical("Trichlor")
+  case Dichlor extends TypeOfChemical("Dichlor")
+  case CalciumHypochlorite extends TypeOfChemical("Calcium Hypochlorite")
+  case Stabilizer extends TypeOfChemical("Stabilizer")
+  case Algaecide extends TypeOfChemical("Algaecide")
+  case MuriaticAcid extends TypeOfChemical("Muriatic Acid")
+  case Salt extends TypeOfChemical("Salt")
+*/
+
 object ChemicalsChart:
   private val dateFormat = DateTimeFormatter.ofPattern("M.dd")
 
-  private def build(model: Model[Chemical], chemical: String): HtmlElement =
+  private def build(model: Model[Chemical], chemical: TypeOfChemical): HtmlElement =
     val dataItems = model
       .entitiesVar
       .now()
-      .filter(c => c.chemical == chemical)
+      .filter(c => c.chemical == chemical.toString)
       .map(c => DataItem( LocalDate.ofEpochDay(c.added).format(dateFormat), c.amount ))
     ChartBuilder.build( Var(dataItems).signal )
+
+  def buildLiquidChlorineChart(model: Model[Chemical]): HtmlElement = build(model, TypeOfChemical.LiquidChlorine)
+
+  def buildTrichlorChart(model: Model[Chemical]): HtmlElement = build(model, TypeOfChemical.Trichlor)
