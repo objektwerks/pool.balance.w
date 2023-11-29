21658adcf3c2cbfca1b5358130b3e41ff479e8c8
objektwerks <objektwerks@runbox.com>
2023-11-29 15:47:21 -0500

    enhanced measurements chart

--- client/src/main/scala/pool/MeasurementsChart.scala
+++ client/src/main/scala/pool/MeasurementsChart.scala
@@ -2,5 +2,9 @@ package pool
 
 import com.raquo.laminar.api.L.*
 
+import java.time.format.DateTimeFormatter
+
 object MeasurementsChart:
+  private val dateFormat = DateTimeFormatter.ofPattern("M.dd")
+
   def build(model: Model[Measurement]): HtmlElement = ???
