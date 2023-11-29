bc6c8718cf6d0e6d55ec7d742a788a351459fee5
objektwerks <objektwerks@runbox.com>
2023-11-29 15:32:11 -0500

    enhanced chemicals chart

--- client/src/main/scala/pool/ChemicalsChart.scala
+++ client/src/main/scala/pool/ChemicalsChart.scala
@@ -2,9 +2,17 @@ package pool
 
 import com.raquo.laminar.api.L.*
 
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+
+import ChartBuilder.DataItem
 
 object ChemicalsChart:
   val dateFormat = DateTimeFormatter.ofPattern("M.dd")
 
-  def build(model: Model[Chemical]): HtmlElement = ???
+  def build(model: Model[Chemical]): HtmlElement =
+    val dataItems = model
+      .entitiesVar
+      .now()
+      .map(c => DataItem( LocalDate.ofEpochDay(c.added).format(dateFormat), c.amount ))
+    ChartBuilder.build( Var(dataItems).signal )
