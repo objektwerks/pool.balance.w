9a39ea88aa4e7d524106cee1311fb0a685370ab0
objektwerks <objektwerks@runbox.com>
2023-11-29 15:53:23 -0500

    edited measurements chart

--- client/src/main/scala/pool/MeasurementsChart.scala
+++ client/src/main/scala/pool/MeasurementsChart.scala
@@ -2,9 +2,30 @@ package pool
 
 import com.raquo.laminar.api.L.*
 
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+
+import ChartBuilder.DataItem
+
+/*
+  totalChlorine: Int = 3,
+  freeChlorine: Int = 3,
+  combinedChlorine: Double = 0.0,
+  ph: Double = 7.4,
+  calciumHardness: Int = 375,
+  totalAlkalinity: Int = 100,
+  cyanuricAcid: Int = 50,
+  totalBromine: Int = 5,
+  salt: Int = 3200,
+  temperature: Int = 85,
+*/
 
 object MeasurementsChart:
   private val dateFormat = DateTimeFormatter.ofPattern("M.dd")
 
-  def build(model: Model[Measurement]): HtmlElement = ???
+  def buildTotalChlorineChart(model: Model[Measurement]): HtmlElement =
+    val dataItems = model
+      .entitiesVar
+      .now()
+      .map(m => DataItem( LocalDate.ofEpochDay(m.measured).format(dateFormat), m.totalChlorine ))
+    ChartBuilder.build( Var(dataItems).signal )
