2bf33b192b20e7831d569966400bee974dec1d52
objektwerks <objektwerks@runbox.com>
2023-11-29 15:24:33 -0500

    enhanced chemicals chart

--- client/src/main/scala/pool/ChemicalsChart.scala
+++ client/src/main/scala/pool/ChemicalsChart.scala
@@ -2,5 +2,9 @@ package pool
 
 import com.raquo.laminar.api.L.*
 
+import java.time.format.DateTimeFormatter
+
 object ChemicalsChart:
+  val dateFormat = DateTimeFormatter.ofPattern("M.dd")
+
   def build(model: Model[Chemical]): HtmlElement = ???
