13cfdccb2c6b5536835931fb81a45b03a359a19f
objektwerks <objektwerks@runbox.com>
2023-11-30 08:46:49 -0500

    moved codecs to command

--- shared/src/main/scala/pool/Command.scala
+++ shared/src/main/scala/pool/Command.scala
@@ -1,6 +1,29 @@
 package pool
 
+import com.github.plokhotnyuk.jsoniter_scala.core.*
+import com.github.plokhotnyuk.jsoniter_scala.macros.*
+
 sealed trait Command
+
+object Command:
+  given JsonValueCodec[Command] = JsonCodecMaker.make[Command]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[License] = JsonCodecMaker.make[License]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[Register] = JsonCodecMaker.make[Register]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[Login] = JsonCodecMaker.make[Login]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[Deactivate] = JsonCodecMaker.make[Deactivate]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[Reactivate] = JsonCodecMaker.make[Reactivate]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[ListPools] = JsonCodecMaker.make[ListPools]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[AddPool] = JsonCodecMaker.make[AddPool]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[UpdatePool] = JsonCodecMaker.make[UpdatePool]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[ListCleanings] = JsonCodecMaker.make[ListCleanings]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[AddCleaning] = JsonCodecMaker.make[AddCleaning]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[UpdateCleaning] = JsonCodecMaker.make[UpdateCleaning]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[ListMeasurements] = JsonCodecMaker.make[ListMeasurements]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[AddMeasurement] = JsonCodecMaker.make[AddMeasurement]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[UpdateMeasurement] = JsonCodecMaker.make[UpdateMeasurement]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[ListChemicals] = JsonCodecMaker.make[ListChemicals]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[AddChemical] = JsonCodecMaker.make[AddChemical]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[UpdateChemical] = JsonCodecMaker.make[UpdateChemical]( CodecMakerConfig.withDiscriminatorFieldName(None) )
 
 sealed trait License:
   val license: String
