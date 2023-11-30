f06d2f7c89f3297484f0d2a8b9a2c33dfeca7bd1
objektwerks <objektwerks@runbox.com>
2023-11-30 08:42:04 -0500

    added codec to account

--- shared/src/main/scala/pool/Entity.scala
+++ shared/src/main/scala/pool/Entity.scala
@@ -49,6 +49,9 @@ final case class Account(id: Long = 0,
                          deactivated: Long = 0) extends Entity:
   def display = emailAddress
 
+object Account:
+  given JsonValueCodec[Account] = JsonCodecMaker.make[Account]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+
 final case class Pool(id: Long = 0,
                       license: String = "",
                       name: String = "", 
