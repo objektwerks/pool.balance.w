29678cef369df70dcffecbf7ad6b11fa718526c8
objektwerks <objektwerks@runbox.com>
2023-11-30 09:10:21 -0500

    added comment

--- client/src/main/scala/pool/MeasurementsChartView.scala
+++ client/src/main/scala/pool/MeasurementsChartView.scala
@@ -8,6 +8,7 @@ object MeasurementsChartView extends View:
   def apply(model: Model[Measurement]): HtmlElement =
     div(
       hdr("Measurements Chart"),
+      // TODO - build chart!
       cbar(
         btn("Measurements").amend {
           onClick --> { _ =>
