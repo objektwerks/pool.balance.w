739ee08bed83aa0944961faefdae204f1adab754
objektwerks <objektwerks@runbox.com>
2023-11-30 07:49:08 -0500

    enhanced cleanings chart view

--- client/src/main/scala/pool/CleaningsChartView.scala
+++ client/src/main/scala/pool/CleaningsChartView.scala
@@ -2,5 +2,10 @@ package pool
 
 import com.raquo.laminar.api.L.*
 
+import Component.*
+
 object CleaningsChartView extends View:
-  def apply(model: Model[Chemical]): HtmlElement = ???
+  def apply(model: Model[Chemical]): HtmlElement =
+    div(
+      hdr("Cleanings Chart")
+    )
