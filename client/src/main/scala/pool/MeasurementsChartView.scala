c253ecc5983e53486a1fb79561cc3c0d1a3e5ad6
objektwerks <objektwerks@runbox.com>
2023-11-30 09:09:35 -0500

    added measurements button to measurements chart view

--- client/src/main/scala/pool/MeasurementsChartView.scala
+++ client/src/main/scala/pool/MeasurementsChartView.scala
@@ -7,5 +7,13 @@ import Component.*
 object MeasurementsChartView extends View:
   def apply(model: Model[Measurement]): HtmlElement =
     div(
-      hdr("Measurements Chart")
+      hdr("Measurements Chart"),
+      cbar(
+        btn("Measurements").amend {
+          onClick --> { _ =>
+            log(s"Measurements Chart -> Measurements button onClick")
+            route(MeasurementsPage)
+          }
+        }
+      )
     )
