3abfdfcafb95c3b8d823b3865b12b311a0dad103
objektwerks <objektwerks@runbox.com>
2023-11-30 07:49:41 -0500

    enhanced measurements chart view

--- client/src/main/scala/pool/MeasurementsChartView.scala
+++ client/src/main/scala/pool/MeasurementsChartView.scala
@@ -2,5 +2,10 @@ package pool
 
 import com.raquo.laminar.api.L.*
 
+import Component.*
+
 object MeasurementsChartView extends View:
-  def apply(model: Model[Chemical]): HtmlElement = ???
+  def apply(model: Model[Chemical]): HtmlElement =
+    div(
+      hdr("Measurements Chart")
+    )
