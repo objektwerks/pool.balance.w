95bff26277b23a94b536c2a5ed509f698555022e
objektwerks <objektwerks@runbox.com>
2023-12-01 06:28:40 -0500

    enhanced measurements chart view

--- client/src/main/scala/pool/MeasurementsChartView.scala
+++ client/src/main/scala/pool/MeasurementsChartView.scala
@@ -5,14 +5,20 @@ import com.raquo.laminar.api.L.*
 import Component.*
 
 object MeasurementsChartView extends View:
-  private val measurements = Measurement.toList
+  private val chartBus = EventBus[HtmlElement]()
 
   def apply(model: Model[Measurement]): HtmlElement =
     div(
       hdr("Measurements Chart"),
       div(
-        listbox(measurements)
-      ),      cbar(
+        listbox(Measurement.toList).amend(
+          onChange.mapToValue --> { value => chartBus.emit( buildChart(value, model) ) }
+        )
+      ),
+      div(
+        child <-- chartBus.events
+      ),
+      cbar(
         btn("Measurements").amend {
           onClick --> { _ =>
             log(s"Measurements Chart -> Measurements button onClick")
