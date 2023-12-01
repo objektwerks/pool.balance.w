0be2085217a40bee336992492f517efeb76d6abb
objektwerks <objektwerks@runbox.com>
2023-12-01 11:51:55 -0500

    enhanced type of measurement test

--- shared/src/test/scala/pool/TypeOfMeasurementTest.scala
+++ shared/src/test/scala/pool/TypeOfMeasurementTest.scala
@@ -8,3 +8,12 @@ import TypeOfMeasurement.*
 final class TypeOfMeasurementTest extends AnyFunSuite with Matchers:
   test("type of measurement"):
     toEnum(TotalChlorine.display) shouldBe TotalChlorine
+    toEnum(FreeChlorine.display) shouldBe FreeChlorine
+    toEnum(CombinedChlorine.display) shouldBe CombinedChlorine
+    toEnum(Ph.display) shouldBe Ph
+    toEnum(CalciumHardness.display) shouldBe CalciumHardness
+    toEnum(TotalAlkalinity.display) shouldBe TotalAlkalinity
+    toEnum(CyanuricAcid.display) shouldBe CyanuricAcid
+    toEnum(TotalBromine.display) shouldBe TotalBromine
+    toEnum(Salt.display) shouldBe Salt
+    toEnum(Temperature.display) shouldBe Temperature
