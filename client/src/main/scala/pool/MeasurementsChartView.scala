40e84088f931f626f9323152b131c26a11558e9c
objektwerks <objektwerks@runbox.com>
2023-12-01 05:23:19 -0500

    refactored measurements chart view

--- client/src/main/scala/pool/MeasurementsChartView.scala
+++ client/src/main/scala/pool/MeasurementsChartView.scala
@@ -5,11 +5,13 @@ import com.raquo.laminar.api.L.*
 import Component.*
 
 object MeasurementsChartView extends View:
+  private val measurements = Measurement.toList
+
   def apply(model: Model[Measurement]): HtmlElement =
     div(
       hdr("Measurements Chart"),
       div(
-        listbox( Measurement.toList )
+        listbox(measurements)
       ),      cbar(
         btn("Measurements").amend {
           onClick --> { _ =>
