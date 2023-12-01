21563bcef88f6cb003fd03441fe7b9a92246e60a
objektwerks <objektwerks@runbox.com>
2023-12-01 06:34:15 -0500

    enhanced measurements chart

--- client/src/main/scala/pool/MeasurementsChartView.scala
+++ client/src/main/scala/pool/MeasurementsChartView.scala
@@ -7,6 +7,19 @@ import Component.*
 object MeasurementsChartView extends View:
   private val chartBus = EventBus[HtmlElement]()
 
+  private def buildChart(selected: String, model: Model[Measurement]): HtmlElement =
+      selected match
+        case "TotalChlorine" => MeasurementsChart.buildTotalChlorineChart(model)
+        case "FreeChlorine" => MeasurementsChart.buildFreeChlorineChart(model)
+        case "CombinedChlorine" => MeasurementsChart.buildCombinedChlorineChart(model)
+        case "pH" => MeasurementsChart.buildPhChart(model)
+        case "CalciumHardness" => MeasurementsChart.buildCalciumHardnessChart(model)
+        case "TotalAlkalinity" => MeasurementsChart.buildTotalAlkalinityChart(model)
+        case "CyanuricAcid" => MeasurementsChart.buildCyanuricAcidChart(model)
+        case "TotalBromine" => MeasurementsChart.buildTotalBromineChart(model)
+        case "Salt" => MeasurementsChart.buildSaltChart(model)
+        case "Temperature" => MeasurementsChart.buildTemperatureChart(model)
+
   def apply(model: Model[Measurement]): HtmlElement =
     div(
       hdr("Measurements Chart"),
