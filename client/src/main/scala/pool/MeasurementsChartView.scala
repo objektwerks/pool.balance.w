099c1ec0c1f1264b8334e649526f4c4a45be6153
objektwerks <objektwerks@runbox.com>
2023-12-01 06:47:03 -0500

    enamed pH to Ph

--- client/src/main/scala/pool/MeasurementsChartView.scala
+++ client/src/main/scala/pool/MeasurementsChartView.scala
@@ -3,28 +3,29 @@ package pool
 import com.raquo.laminar.api.L.*
 
 import Component.*
+import TypeOfMeasurement.*
 
 object MeasurementsChartView extends View:
   private val chartBus = EventBus[HtmlElement]()
 
   private def buildChart(selected: String, model: Model[Measurement]): HtmlElement =
-      selected match
-        case "TotalChlorine" => MeasurementsChart.buildTotalChlorineChart(model)
-        case "FreeChlorine" => MeasurementsChart.buildFreeChlorineChart(model)
-        case "CombinedChlorine" => MeasurementsChart.buildCombinedChlorineChart(model)
-        case "pH" => MeasurementsChart.buildPhChart(model)
-        case "CalciumHardness" => MeasurementsChart.buildCalciumHardnessChart(model)
-        case "TotalAlkalinity" => MeasurementsChart.buildTotalAlkalinityChart(model)
-        case "CyanuricAcid" => MeasurementsChart.buildCyanuricAcidChart(model)
-        case "TotalBromine" => MeasurementsChart.buildTotalBromineChart(model)
-        case "Salt" => MeasurementsChart.buildSaltChart(model)
-        case "Temperature" => MeasurementsChart.buildTemperatureChart(model)
+      TypeOfMeasurement.toEnum(selected) match
+        case TotalChlorine => MeasurementsChart.buildTotalChlorineChart(model)
+        case FreeChlorine => MeasurementsChart.buildFreeChlorineChart(model)
+        case CombinedChlorine => MeasurementsChart.buildCombinedChlorineChart(model)
+        case Ph => MeasurementsChart.buildPhChart(model)
+        case CalciumHardness => MeasurementsChart.buildCalciumHardnessChart(model)
+        case TotalAlkalinity => MeasurementsChart.buildTotalAlkalinityChart(model)
+        case CyanuricAcid => MeasurementsChart.buildCyanuricAcidChart(model)
+        case TotalBromine => MeasurementsChart.buildTotalBromineChart(model)
+        case Salt => MeasurementsChart.buildSaltChart(model)
+        case Temperature => MeasurementsChart.buildTemperatureChart(model)
 
   def apply(model: Model[Measurement]): HtmlElement =
     div(
       hdr("Measurements Chart"),
       div(
-        listbox(Measurement.toList).amend(
+        listbox(TypeOfMeasurement.toList).amend(
           onChange.mapToValue --> { value => chartBus.emit( buildChart(value, model) ) }
         )
       ),
