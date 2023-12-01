f9ff8e3ee15024dc122e60cbd6c99675852bb1bf
objektwerks <objektwerks@runbox.com>
2023-12-01 05:16:03 -0500

    enhanced measurements chart view

--- client/src/main/scala/pool/MeasurementsChartView.scala
+++ client/src/main/scala/pool/MeasurementsChartView.scala
@@ -8,8 +8,9 @@ object MeasurementsChartView extends View:
   def apply(model: Model[Measurement]): HtmlElement =
     div(
       hdr("Measurements Chart"),
-      // TODO - build chart!
-      cbar(
+      div(
+        listbox( List() )
+      ),      cbar(
         btn("Measurements").amend {
           onClick --> { _ =>
             log(s"Measurements Chart -> Measurements button onClick")
