c40986c00ff1fe47c606c8f839fa7f10cacdbe49
objektwerks <objektwerks@runbox.com>
2024-01-18 06:18:12 -0500

    added cors support

--- server/src/main/scala/pool/Server.scala
+++ server/src/main/scala/pool/Server.scala
@@ -3,7 +3,9 @@ package pool
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
+import io.helidon.cors.CrossOriginConfig
 import io.helidon.webserver.WebServer
+import io.helidon.webserver.cors.CorsSupport
 import io.helidon.webserver.http.HttpRouting
 
 import scala.concurrent.duration.DurationInt
@@ -19,11 +21,25 @@ object Server extends LazyLogging:
     val emailer = Emailer(config)
     val dispatcher = Dispatcher(store, emailer)
 
+    val cors = CorsSupport
+      .builder()  
+      .addCrossOrigin(
+        CrossOriginConfig
+          .builder()
+          .allowCredentials(true)
+          .allowHeaders("*")
+          .allowMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+          .allowOrigins("http://localhost")
+          .build()
+      ) 
+      .addCrossOrigin(CrossOriginConfig.create())
+      .build()
+
     val handler = Handler(dispatcher, store, logger)
 
     val builder = HttpRouting
       .builder
-      .get(endpoint, handler)
+      .get(endpoint, cors, handler)
 
     WebServer
       .builder
