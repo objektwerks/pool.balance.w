f2d7b6ddeee325108c0a9d8c2d2e93a5558dfd6f
objektwerks <objektwerks@runbox.com>
2024-07-24 10:20:45 -0400

    added security to dispatcher

--- server/src/main/scala/pool/Dispatcher.scala
+++ server/src/main/scala/pool/Dispatcher.scala
@@ -7,6 +7,9 @@ import scala.util.control.NonFatal
 
 import Validator.*
 
+sealed trait Security
+case object Authorized extends Security
+final case class Unauthorized(cause: String) extends Security
 
 final class Dispatcher(store: Store, emailer: Emailer):
   def dispatch[E <: Event](command: Command): Event =
