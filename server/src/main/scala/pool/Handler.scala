3a6b80e7897750711ddc73a8638372eff60f82e7
objektwerks <objektwerks@runbox.com>
2024-07-24 10:38:54 -0400

    refactored handler

--- server/src/main/scala/pool/Handler.scala
+++ server/src/main/scala/pool/Handler.scala
@@ -1,27 +1,24 @@
 package pool
 
 import com.github.plokhotnyuk.jsoniter_scala.core.*
-import com.typesafe.scalalogging.Logger
+import com.typesafe.scalalogging.LazyLogging
 
 import io.helidon.webserver.http.{Handler => WebHandler, ServerRequest, ServerResponse}
 
-final class Handler(dispatcher: Dispatcher,
-                    store: Store,
-                    logger: Logger) extends WebHandler:
+final class Handler(dispatcher: Dispatcher) extends WebHandler with LazyLogging:
   override def handle(request: ServerRequest,
                       response: ServerResponse): Unit =
     val commandJson = request.content.as(classOf[String])
+    logger.info(s"*** Handler command json: $commandJson")
+
     val command = readFromString[Command](commandJson)
     logger.debug(s"*** Handler command: $command")
 
     val event = dispatcher.dispatch(command)
     logger.debug(s"*** Handler event: $event")
-    event match
-      case fault @ Fault(_, _) =>
-        logger.error(s"*** Handler fault: $fault")
-        store.addFault(fault)
-      case _ =>
+
     val eventJson = writeToString[Event](event)
+    logger.debug(s"*** Handler json: $eventJson")
 
     response
       .status(200)
