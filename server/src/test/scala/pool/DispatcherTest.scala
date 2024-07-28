0cb65e3613f23ea4ab9519aafe2fd0b7bd024db0
objektwerks <objektwerks@runbox.com>
2024-07-28 12:37:37 -0400

    adapted test to new store config constructor

--- server/src/test/scala/pool/DispatcherTest.scala
+++ server/src/test/scala/pool/DispatcherTest.scala
@@ -14,10 +14,7 @@ final class DispatcherTest extends AnyFunSuite with Matchers:
   exitCode shouldBe 0
 
   val config = ConfigFactory.load("test.conf")
-
-  val cache = Store.cache(config)
-  val dataSource = Store.dataSource(config)
-  val store = Store(cache, dataSource)
+  val store = Store(config)
   val emailer = Emailer(config)
   val dispatcher = Dispatcher(store, emailer)
 
