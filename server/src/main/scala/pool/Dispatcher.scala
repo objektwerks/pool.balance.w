b77961947f651d5cd24b5bce0251b537010f3638
objektwerks <objektwerks@runbox.com>
2024-08-29 10:45:18 -0400

    refactored list measurements

--- server/src/main/scala/pool/Dispatcher.scala
+++ server/src/main/scala/pool/Dispatcher.scala
@@ -155,11 +155,15 @@ final class Dispatcher(store: Store, emailer: Emailer):
     }.recover { case NonFatal(error) => Fault(s"Update cleaning failed:", error) }
      .get
 
-  private def listMeasurements(poolId: Long): Event =
-    Try {
-      MeasurementsListed( store.listMeasurements(poolId) )
-    }.recover { case NonFatal(error) => Fault(s"List measurements failed:", error) }
-     .get
+  private def listMeasurements(poolId: Long)(using IO): Event =
+    Try:
+      MeasurementsListed(
+        supervised:
+          retry( RetryConfig.delay(1, 100.millis) )( store.listMeasurements(poolId) )
+      )
+    .recover:
+      case NonFatal(error) => Fault("List measurements failed:", error)
+    .get
 
   private def addMeasurement(measurement: Measurement): Event =
     Try {
