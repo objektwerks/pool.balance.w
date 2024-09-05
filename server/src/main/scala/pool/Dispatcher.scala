a5809c045645e24c30576602a1d2dd9a357e75b6
objektwerks <objektwerks@runbox.com>
2024-09-05 14:04:14 -0400

    edited dispatcher

--- server/src/main/scala/pool/Dispatcher.scala
+++ server/src/main/scala/pool/Dispatcher.scala
@@ -131,14 +131,13 @@ final class Dispatcher(store: Store, emailer: Emailer):
       case NonFatal(error) => Fault("Update pool failed:", error)
 
   private def listCleanings(poolId: Long)(using IO): Event =
-    Try:
+    try
       CleaningsListed(
         supervised:
           retry( RetryConfig.delay(1, 100.millis) )( store.listCleanings(poolId) )
       )
-    .recover:
+    catch
       case NonFatal(error) => Fault("List cleanings failed:", error)
-    .get
 
   private def addCleaning(cleaning: Cleaning)(using IO): Event =
     Try:
